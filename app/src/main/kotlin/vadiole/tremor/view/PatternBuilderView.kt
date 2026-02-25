55d5e30baed2fdfeaf504a76a37bfcf807565206
vadiole <xdmitree@gmail.com>
2026-02-25 02:41:36 +0100

    fix build errors in pattern builder and wave overlay

--- app/src/main/kotlin/vadiole/tremor/view/PatternBuilderView.kt
+++ app/src/main/kotlin/vadiole/tremor/view/PatternBuilderView.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Typeface
 import android.view.Gravity
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
