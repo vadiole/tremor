0bb2b38fc96cc3edbeb3a23496d1851753c36b3d
vadiole <xdmitree@gmail.com>
2026-03-24 20:23:48 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "vadiole.tremor"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         base.archivesName.set("tremor-v$versionName")
     }
     androidResources {
